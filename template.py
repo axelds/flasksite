8ba8fa92c732c017869f7832ae7c2789b83da10c
axelds <52823253+axelds@users.noreply.github.com>
2020-11-23 15:10:15 +0200

    Update template.py

--- template.py
+++ template.py
@@ -1,24 +1,25 @@
+import os
 from flask import * 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    user = "Александр"
-    return render_template('index.html', username = user, title='Домашняя страница')
+    user = "РђР»РµРєСЃР°РЅРґСЂ"
+    return render_template('index.html', username = user, title='Р”РѕРјР°С€РЅСЏСЏ СЃС‚СЂР°РЅРёС†Р°')
 @app.route('/news')
 def news():
     with open("templates/news.json", "rt", encoding="utf8") as f:
         news_list = json.loads(f.read())
-    return render_template('news.html', news=news_list, title='Новости')
+    return render_template('news.html', news=news_list, title='РќРѕРІРѕСЃС‚Рё')
 
 @app.route('/photo')
 def first():
     photo = url_for('static', filename='images/picture.png')
-    return render_template('picture.html', photo = photo, title='Картинка') 
+    return render_template('picture.html', photo = photo, title='РљР°СЂС‚РёРЅРєР°') 
 
 @app.route('/register', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
-        return render_template('register.html', title='Регистрация')
+        return render_template('register.html', title='Р РµРіРёСЃС‚СЂР°С†РёСЏ')
     elif request.method == 'POST':
         print(request.form.get('email'))
         print(request.form.get('password'))
@@ -26,7 +27,7 @@ def form_sample():
         print(request.form.get('about'))
         print(request.form.get('accept'))
         print(request.form.get('sex'))
-        return render_template('thankyou.html', title='Регистрация')
+        return render_template('thankyou.html', title='Р РµРіРёСЃС‚СЂР°С†РёСЏ')
 
 
 if __name__ == '__main__':
